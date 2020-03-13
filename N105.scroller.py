2bced613bcb1fe2d026fc0a222c6d99e40a114db
Hundredvisionsguy <cwinikka@gmail.com>
2020-03-12 17:36:40 -0700

    adding N105 scroller and other sample files

---
+++ N105.scroller.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import time
+from samplebase import SampleBase
+from PIL import Image
+import day_finder
+from datetime import date
+
+
+class ImageScroller(SampleBase):
+    def __init__(self, *args, **kwargs):
+        super(ImageScroller, self).__init__(*args, **kwargs)
+        self.parser.add_argument("-i", "--image", help="The image to display", default="../../../examples-api-use/runtext.ppm")
+
+    def run(self):
+        # Get the Day
+        day = day_finder.get_current_day()
+        # Get hour and minute using time
+        today = date.today()
+        #c_hour = today.
+        if day == "DAY 2":
+            self.image = Image.open("LEDWelcomeVGD.png").convert('RGB')
+        elif not 'image' in self.__dict__:
+            self.image = Image.open(self.args.image).convert('RGB')
+        self.image.resize((self.matrix.width, self.matrix.height), Image.ANTIALIAS)
+
+        double_buffer = self.matrix.CreateFrameCanvas()
+        img_width, img_height = self.image.size
+
+        # let's scroll
+        xpos = 0
+        while True:
+            xpos += 1
+            if (xpos > img_width):
+                xpos = 0
+
+            double_buffer.SetImage(self.image, -xpos)
+            double_buffer.SetImage(self.image, -xpos + img_width)
+
+            double_buffer = self.matrix.SwapOnVSync(double_buffer)
+            time.sleep(0.01)
+
+# Main function
+# e.g. call with
+#  sudo ./image-scroller.py --chain=4
+# if you have a chain of four
+if __name__ == "__main__":
+    image_scroller = ImageScroller()
+    if (not image_scroller.process()):
+        image_scroller.print_help()
