31f5a47cd3f3b7aaf4ff337341df41ae13e33945
hundredvisionsguy <cwinikka@gmail.com>
2020-04-18 17:11:13 -0700

    renaming image-scroller and adjusting test_cmd

--- test_cmd.py
+++ test_cmd.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import time
+import image_scroller
 
 #os.system("echo Hello from the other side!")
 #os.system(".py --led-cols=64 --led-rows=32 -b 25 -t 'Welcome to the hom$
