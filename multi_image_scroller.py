00d4d146a687c53db30e5a1df7bbd2c240777a34
Hundredvisionsguy <cwinikka@gmail.com>
2020-04-27 19:25:14 -0700

    adding multiple image scroller and Nerd Alert graphic

--- multi_image_scroller.py
+++ multi_image_scroller.py
@@ -24,7 +24,7 @@ class ImageScroller(SampleBase):
         last_minute = minute
         # let's scroll
         xpos = 0
-        #switch = 0
+        switch = 0
         while True:
             # get current minute
             minute = now.strftime("%M")
@@ -33,21 +33,21 @@ class ImageScroller(SampleBase):
                 xpos = 0
                 # check if a minute has passed
                 # if so, it's time to switch images
-                #if switch == 0:
-                #    self.image = Image.open("Nerd_Alert.png").convert('RGB')
-                #elif switch == 1:
-                #    self.image = Image.open("LEDWelcomeFamily.png").convert('RGB')
-                #elif switch == 2:
-                #    self.image = Image.open("LEDWelcomeVGD.png").convert('RGB')
-                #elif switch == 3:
-                #    self.image = Image.open("LEDWelcomeP1.png").convert('RGB')
-                #else:
-                #    self.image = Image.open("you_got_this.png").convert('RGB')
-                #self.image.resize((self.matrix.width, self.matrix.height), Image.ANTIALIAS)
-                #double_buffer = self.matrix.CreateFrameCanvas()
-                #img_width, img_height = self.image.size
-                #switch += 1
-                #if switch > 4: switch = 0
+                if switch == 0:
+                    self.image = Image.open("Nerd_Alert.png").convert('RGB')
+                elif switch == 1:
+                    self.image = Image.open("LEDWelcomeFamily.png").convert('RGB')
+                elif switch == 2:
+                    self.image = Image.open("LEDWelcomeVGD.png").convert('RGB')
+                elif switch == 3:
+                    self.image = Image.open("LEDWelcomeP1.png").convert('RGB')
+                else:
+                    self.image = Image.open("you_got_this.png").convert('RGB')
+                self.image.resize((self.matrix.width, self.matrix.height), Image.ANTIALIAS)
+                double_buffer = self.matrix.CreateFrameCanvas()
+                img_width, img_height = self.image.size
+                switch += 1
+                if switch > 4: switch = 0
                 #double_buffer = self.matrix.CreateFrameCanvas()
                 #img_width, img_height = self.image.size
 
@@ -55,7 +55,7 @@ class ImageScroller(SampleBase):
             double_buffer.SetImage(self.image, -xpos + img_width)
             double_buffer = self.matrix.SwapOnVSync(double_buffer)
             time.sleep(0.01)
-            #last_minute = minute
+            last_minute = minute
 
 # Main function
 # e.g. call with
